eea585120e8434bfba52a8d0d4a0bad4ad2a08b2
Arias71 <alejandro.arias@est.iudigital.edu.co>
2025-03-16 12:24:45 -0500

    Add files via upload

---
+++ Pad_2025/configuracion.py
@@ -0,0 +1,21 @@
+
+from setuptools import setup,find_packages
+
+setup (
+    name= "sindy_baquero",
+    version= "0.0.1",
+    author= "Alejandro_Arias,
+    author_email= "sindy.baquero@est.iudigital.edu.co",
+    description= "",
+    py_modules= ["Entregable actividad 1 y 2"],
+    install_requires= [
+        "kagglehub[pandas-datasets]>=0.3.8",
+        "matplotlib>=3.5.0",
+        "seaborn>=0.11.2",
+        "pandas",
+        "numpy",
+        "matplotlib",
+        "openpyxl",
+        "requests"
+ ]
+ )
